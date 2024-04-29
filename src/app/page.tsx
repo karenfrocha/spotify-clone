06d374cad435fa49782e49ba3b91d5d5b92ee827
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-29 20:53:11 -0300

    refactor: add elements to the side menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,22 @@ export default function Home() {
             <span>Buscar</span>
           </div>
         </div>
-        <div className="flex flex-col w-full h-[45.56rem] rounded-lg bg-[#121212]"></div>
+
+        <div className="flex flex-col gap-2 py-2 px-3 w-full h-[45.56rem] rounded-lg bg-[#121212]">
+          <div className="flex flex-row w-full h-10 gap-4 py-1 px-3 items-center justify-start text-[#a7a7a7] hover:text-white cursor-pointer font-bold text-base">
+            <span className="material-icons-outlined text-3xl">dehaze</span>
+            <span>Sua biblioteca</span>
+            <button className="material-icons-outlined text-3xl">add</button>
+            <button className="material-icons-outlined text-3xl">
+              arrowforward
+            </button>
+          </div>
+          <div className="flex flex-col gap-4 py-16 px-20 rounded-lg bg-[#242424]">
+            <h1 className="text-[#ffff]">Crie sua primeira playlist</h1>
+            <h2 className="text-[#ffff]">É fácil, vamos te ajudar</h2>
+            <button></button>
+          </div>
+        </div>
       </div>
       <div></div>
     </div>
