440ebb68acaa9e12e0ea73c0d83d4cc3070bf3b7
Karen Félix <karen.felix.rocha@gmail.com>
2024-05-06 20:42:15 -0300

    refactor: improve home page with finished sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,61 @@
-import Image from "next/image";
+import { YourLibraryContentItem } from "../components";
 
 export default function Home() {
   return (
     <div className="flex flex-row w-screen h-screen p-2 bg-black">
+      {/* Side Navigation */}
       <div className="flex flex-col w-[26.25rem] h-full gap-2">
+        {/* Menu */}
         <div className="flex flex-col gap-2 py-2 px-3 w-full h-28 rounded-lg bg-[#121212] items-center justify-center">
+          {/* Menu Item*/}
           <div className="flex flex-row w-full h-10 gap-4 py-1 px-3 items-center justify-start text-[#a7a7a7] hover:text-white cursor-pointer font-bold text-base">
             <span className="material-icons-outlined text-3xl">home</span>
             <span>Início</span>
           </div>
+          {/* Menu Item*/}
           <div className="flex flex-row w-full h-10 gap-4 py-1 px-3 items-center justify-start text-[#a7a7a7] hover:text-white cursor-pointer font-bold text-base">
             <span className="material-icons-outlined text-3xl">search</span>
             <span>Buscar</span>
           </div>
         </div>
 
+        {/* Your Library */}
         <div className="flex flex-col gap-2 py-2 px-3 w-full h-[45.56rem] rounded-lg bg-[#121212]">
-          <div className="flex flex-row w-full h-10 gap-4 py-1 px-3 items-center justify-start text-[#a7a7a7] hover:text-white cursor-pointer font-bold text-base">
-            <span className="material-icons-outlined text-3xl">dehaze</span>
-            <span>Sua biblioteca</span>
-            <button className="material-icons-outlined text-3xl">add</button>
-            <button className="material-icons-outlined text-3xl">
-              arrowforward
-            </button>
+          {/* Your Library Header */}
+          <div className="flex flex-row w-full h-10 gap-4 py-1 px-3 items-center justify-between text-[#a7a7a7] font-bold text-base">
+            <div className="flex flex-row items-center gap-2 hover:text-white cursor-pointer">
+              <span className="material-icons-outlined text-3xl">dehaze</span>
+              <span>Sua Biblioteca</span>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <button className="flex justify-center items-center p-2 size-8 hover:text-white hover:bg-white/5 rounded-full cursor-pointer">
+                <span className="material-icons-outlined size-4">add</span>
+              </button>
+              <button className="flex justify-center items-center p-2 size-8 hover:text-white hover:bg-white/5 rounded-full cursor-pointer">
+                <span className="material-icons-outlined size-4">
+                  arrow_forward
+                </span>
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col gap-4 py-4 px-5 w-full h-[8.4rem] rounded-lg bg-[#242424]">
-            <h1 className="text-[#ffff]">Crie sua primeira playlist</h1>
-            <h2 className="text-[#ffff]">É fácil, vamos te ajudar.</h2>
-            <button className="bg-[#ffff] text-black rounded-full">Criar playlist</button>
-          </div>
-          <div className="flex flex-col gap-4 py-4 px-5 w-full h-[8.4rem] rounded-lg bg-[#242424]">
-            <h1 className="text-[#ffff]">Que tal seguir um podcast novo?</h1>
-            <h2 className="text-[#ffff]">Avisaremos você sobre novos episódios.</h2>
-            <button className="bg-[#ffff] text-black rounded-full gap-5">Explore podcasts</button>
+
+          {/* Your Library Content */}
+          <div className="flex flex-col w-full items-start justify-center gap-6">
+            <YourLibraryContentItem
+              title="Crie sua primeira playlist"
+              description="É fácil, vamos te ajudar."
+              buttonLabel="Criar playlist"
+            />
+
+            <YourLibraryContentItem
+              title="Que tal seguir um podcast novo?"
+              description="Avisaremos você sobre novos episódios."
+              buttonLabel="Explore podcasts"
+            />
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="flex flex-row w-screen h-screen p-2 bg-[#242424]"></div>
     </div>
   );
 }
